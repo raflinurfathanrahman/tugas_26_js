3fd40e3876253aa76a8873a3da4f4a0fa066f8f7
raflirahman <raflirahman375@gmail.com>
2022-10-06 13:30:19 +0700

    add rugas 26

---
+++ tugas26.js
@@ -0,0 +1,113 @@
+function data(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([0])) {
+        return 'Angka 2 NOT Infinity.';
+      }else{
+      return 'Angka 2 is Infinity!';
+    }     
+    }  
+console.log(data());
+
+function data2(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([1])) {
+        return 'Angka 39 NOT Infinity.';
+      }else{
+      return 'Angka 39 is Infinity!';
+    }     
+    }  
+console.log(data2());
+
+function data3(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([2])) {
+        return 'Angka 76 NOT Infinity.';
+      }else{
+      return 'Angka 76 is Infinity!';
+    }     
+    }  
+console.log(data3());
+
+function data4(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([3])) {
+        return 'Angka 50 NOT Infinity.';
+      }else{
+      return 'Angka 50 is Infinity!';
+    }     
+    }  
+console.log(data4())
+
+function data5(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([4])) {
+        return 'Angka 9 NOT Infinity.';
+      }else{
+      return 'Angka 9 is Infinity!';
+    }     
+    }  
+console.log(data5())
+
+function data6(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([5])) {
+        return 'Angka 7 NOT Infinity.';
+      }else{
+      return 'Angka 7 is Infinity!';
+    }     
+    }  
+console.log(data6())
+
+function data7(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([6])) {
+        return 'Angka 41 NOT Infinity.';
+      }else{
+      return 'Angka 41 is Infinity!';
+    }     
+    }  
+console.log(data7())
+
+function data8(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([7])) {
+        return 'Angka 2 NOT Infinity.';
+      }else{
+      return 'Angka 2 is Infinity!';
+    }     
+    }  
+console.log(data8())
+
+function data9(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([8])) {
+        return 'Angka 24 NOT Infinity.';
+      }else{
+      return 'Angka 24 is Infinity!';
+    }     
+    }  
+console.log(data9())
+
+function data10(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([9])) {
+        return 'Angka 1 NOT Infinity.';
+      }else{
+      return 'Angka 1 is Infinity!';
+    }     
+    }  
+console.log(data10())
+
+function data11(){
+    angka = isFinite([2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]);
+    if (isFinite([10])) {
+        return 'Angka 16 NOT Infinity.';
+      }else{
+      return 'Angka 16 is Infinity!';
+    }     
+    }  
+console.log(data11())
+
+
+
+
